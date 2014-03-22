7e06ce8da36ba1a91b35b06e2d0a13f304d4a603
pjindracek <pjindracek@gmail.com>
2014-03-22 13:53:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Togather::Application.config.secret_key_base = '064d226bad5f06dcdd2855130b1bf3bd163d98081dbe84fbd13466db1487be52515eb410d16f04dbbec854c9877401576dfff2c838d115f5544fcad418f44538'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Togather::Application.config.session_store :cookie_store, key: '_togather_session'
